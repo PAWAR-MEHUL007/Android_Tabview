3a85ba5082c77bb90d8cc92c40e78f98b1bc8dc3
PAWAR-MEHUL007 <mpavar007@gmail.com>
2021-09-30 10:42:12 -0600

    Some code add in MainActivity.java

--- app/src/main/java/com/example/tabview/MainActivity.java
+++ app/src/main/java/com/example/tabview/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -16,13 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TabLayout tabLayout = null;
-
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        loadFragment(new FragmentOne());
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
 
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-
+                Log.d("tabs","" + tab.getPosition());
+                if(tab.getPosition()==0)
+                {
+                    loadFragment(new FragmentOne());
+                }
+                else if(tab.getPosition()==1)
+                {
+                    loadFragment(new FragmentTwo());
+                }
             }
 
             @Override
